dd3094a1be4b9f17525902c3aa7619d15799f551
chandankrishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2022-08-07 14:33:06 +0530

    Math pow function fmt print package

---
+++ matPow.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	x := 5.3
+	result := math.Pow(x, 8)
+	fmt.Println("abc ", result)
+
+}
